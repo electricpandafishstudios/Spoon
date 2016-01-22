48e93751b2bf3a467e65a6a6ee5d0758f708e159
Christopher Evans <Christopher Evans>
2016-01-22 16:35:35 -0500

    Added custom "spoon cell" class that has fireball ability (reskinned acid). ATCG stats added, unsure where stats are used in the example. Using example dialogue.
    Signed-off-by: Christopher Evans <Christopher Evans>

---
+++ game/modules/Spoon/init.lua
@@ -0,0 +1,33 @@
+-- ToME - Tales of Middle-Earth
+-- Copyright (C) 2009 - 2015 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+name = "Spoon Module"
+long_name = "Spoon the game"
+short_name = "spoon"
+author = { "people" }
+homepage = "N/A"
+version = {0,0,1}
+engine = {1,3,3,"te4"}
+description = [[
+Spoon Game pre-pre-alpha!
+]]
+no_get_name = true
+allow_userchat = false
+starter = "mod.load"
+show_only_on_cheat = false -- Example modules are not shown to normal players
