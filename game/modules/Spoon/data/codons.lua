ae936030afd56f63ecc67677613bdc0a85de17a1
Spencer Brower <foxtrotalphalima@gmail.com>
2016-01-28 15:12:37 -0500

    Added Codons

--- game/modules/Spoon/data/codons.lua
+++ game/modules/Spoon/data/codons.lua
@@ -2,12 +2,34 @@ newCodonType{ type = "normal", name = "hp", description = "raises hp"}
 
 newCodon{
 	name = "HP",
-	type = {"normal", 1},
+	type = {"normal"},
 	on_gain = function(self, c)
 		self.max_life = self.max_life + 1
 		self.life = self.max_life
 	end,
 	info = function(self, c)
 		return "Ups HP."
+	end,
+}
+
+newCodon{
+	name = "Dam",
+	type = {"normal"},
+	on_gain = function(self, c)
+		self.dam = self.dam + 1
+	end,
+	info = function(self, c)
+		return "Ups damage."
+	end,
+}
+
+newCodon{
+	name = "Fire ball",
+	type = {"normal"},
+	on_gain = function(self, c)
+		self:learnTalent("T_FIRE_BALL", true, 1)
+	end,
+	info = function(self, c)
+		return "Ups damage."
 	end,
 }
