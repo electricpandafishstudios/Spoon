6ce6e9ad152731ed678b405a13f8ea87425f5473
Spencer Brower <foxtrotalphalima@gmail.com>
2016-02-05 17:24:14 -0500

    rearranged code

--- game/modules/Spoon/dialogs/LevelUp.lua
+++ game/modules/Spoon/dialogs/LevelUp.lua
@@ -17,36 +17,28 @@ function _M:init(actor)
 
 	self:generateList()
 
-	self.c_1_1 = Button.new{can_focus = false, can_focus_mouse=true, text="UUU", fct=function() if self:canUse(0,3,0,0) then self:use("HP") else self:cantUse() end end, on_select=function()
+	self.c_1_1 = Button.new{can_focus = false, can_focus_mouse=true, text="UUU", fct=function() if self:canUse(3,0,0,0) then self:use("HP") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
 			self.c_desc:switchItem(str, str, true)
 		end
 	end}
-	self.c_1_2 = Button.new{can_focus = false, can_focus_mouse=true, text="UUC", fct=function() if self:canUse(0,2,1,0) then self:use("HP") else self:cantUse() end end, on_select=function()
+	self.c_1_2 = Button.new{can_focus = false, can_focus_mouse=true, text="UUC", fct=function() if self:canUse(2,1,0,0) then self:use("HP") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
 			self.c_desc:switchItem(str, str, true)
 		end
 	end}
-	self.c_1_3 = Button.new{can_focus = false, can_focus_mouse=true, text="UUA", fct=function() if self:canUse(1,2,0,0) then self:use("HP") else self:cantUse() end end, on_select=function()
+	self.c_1_3 = Button.new{can_focus = false, can_focus_mouse=true, text="UUA", fct=function() if self:canUse(2,0,1,0) then self:use("HP") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
 			self.c_desc:switchItem(str, str, true)
 		end
 	end}
-	self.c_1_4 = Button.new{can_focus = false, can_focus_mouse=true, text="UUG", fct=function() if self:canUse(0,2,0,1) then self:use("HP") else self:cantUse() end end, on_select=function()
-		local str = desc_types
-		if self.no_tooltip then
-			self.c_desc:erase()
-			self.c_desc:switchItem(str, str, true)
-		
-		end
-	end}
-	self.c_1_5 = Button.new{can_focus = false, can_focus_mouse=true, text="CUU", fct=function() if self:canUse(0,2,1,0) then self:use("HP") else self:cantUse() end end, on_select=function()
+	self.c_1_4 = Button.new{can_focus = false, can_focus_mouse=true, text="UUG", fct=function() if self:canUse(2,0,0,1) then self:use("HP") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -54,7 +46,15 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.c_1_6 = Button.new{can_focus = false, can_focus_mouse=true, text="CUC", fct=function() if self:canUse(0,1,2,0) then self:use("HP") else self:cantUse() end end, on_select=function()
+	self.c_1_5 = Button.new{can_focus = false, can_focus_mouse=true, text="CUU", fct=function() if self:canUse(2,1,0,0) then self:use("HP") else self:cantUse() end end, on_select=function()
+		local str = desc_types
+		if self.no_tooltip then
+			self.c_desc:erase()
+			self.c_desc:switchItem(str, str, true)
+		
+		end
+	end}
+	self.c_1_6 = Button.new{can_focus = false, can_focus_mouse=true, text="CUC", fct=function() if self:canUse(2,1,0,0) then self:use("HP") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -70,7 +70,7 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.c_2_2 = Button.new{can_focus = false, can_focus_mouse=true, text="CUG",fct=function() if self:canUse(0,1,1,1) then self:use("DAM") else self:cantUse() end end, on_select=function()
+	self.c_2_2 = Button.new{can_focus = false, can_focus_mouse=true, text="CUG",fct=function() if self:canUse(1,1,0,1) then self:use("DAM") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -78,7 +78,7 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.c_2_3 = Button.new{can_focus = false, can_focus_mouse=true, text="AUU", fct=function() if self:canUse(2,1,0,0) then self:use("DAM") else self:cantUse() end end, on_select=function()
+	self.c_2_3 = Button.new{can_focus = false, can_focus_mouse=true, text="AUU", fct=function() if self:canUse(2,0,1,0) then self:use("DAM") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -94,7 +94,7 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.c_2_5 = Button.new{can_focus = false, can_focus_mouse=true, text="AUA", fct=function() if self:canUse(2,1,0,0) then self:use("DAM") else self:cantUse() end end, on_select=function()
+	self.c_2_5 = Button.new{can_focus = false, can_focus_mouse=true, text="AUA", fct=function() if self:canUse(1,0,2,0) then self:use("DAM") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -102,7 +102,7 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.c_2_6 = Button.new{can_focus = false, can_focus_mouse=true, text="AUG", fct=function() if self:canUse(1,1,0,1) then self:use("DAM") else self:cantUse() end end, on_select=function()
+	self.c_2_6 = Button.new{can_focus = false, can_focus_mouse=true, text="AUG", fct=function() if self:canUse(1,0,1,1) then self:use("DAM") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -110,7 +110,7 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.u_1_1 = Button.new{can_focus = false, can_focus_mouse=true, text="GUU", fct=function() if self:canUse(0,2,0,1) then self:use("FB") else self:cantUse() end end, on_select=function()
+	self.u_1_1 = Button.new{can_focus = false, can_focus_mouse=true, text="GUU", fct=function() if self:canUse(2,0,0,1) then self:use("FB") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -118,7 +118,7 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.u_1_2 = Button.new{can_focus = false, can_focus_mouse=true, text="GUC", fct=function() if self:canUse(0,1,1,1) then self:use("FB") else self:cantUse() end end, on_select=function()
+	self.u_1_2 = Button.new{can_focus = false, can_focus_mouse=true, text="GUC", fct=function() if self:canUse(1,1,0,1) then self:use("FB") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -126,7 +126,7 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.u_1_3 = Button.new{can_focus = false, can_focus_mouse=true, text="GUA", fct=function() if self:canUse(1,1,0,1) then self:use("FB") else self:cantUse() end end, on_select=function()
+	self.u_1_3 = Button.new{can_focus = false, can_focus_mouse=true, text="GUA", fct=function() if self:canUse(1,0,1,1) then self:use("FB") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -134,7 +134,7 @@ function _M:init(actor)
 		
 		end
 	end}
-	self.u_1_4 = Button.new{can_focus = false, can_focus_mouse=true, text="GUG", fct=function() if self:canUse(0,1,0,2) then self:use("FB") else self:cantUse() end end, on_select=function()
+	self.u_1_4 = Button.new{can_focus = false, can_focus_mouse=true, text="GUG", fct=function() if self:canUse(1,0,0,2) then self:use("FB") else self:cantUse() end end, on_select=function()
 		local str = desc_types
 		if self.no_tooltip then
 			self.c_desc:erase()
@@ -186,16 +186,16 @@ function _M:init(actor)
 	self:setFocus(self.c_1_1)
 	self:setupUI(false, true)
 end
-function _M:canUse(A,U,C,G)
+function _M:canUse(U,C,A,G)
 	
-	if self.actor:getA() < A then return false end
 	if self.actor:getU() < U then return false end
 	if self.actor:getC() < C then return false end
+	if self.actor:getA() < A then return false end
 	if self.actor:getG() < G then return false end
 	
-	self.actor:incStat(game.player.STAT_A, -A)
 	self.actor:incStat(game.player.STAT_U, -U)
 	self.actor:incStat(game.player.STAT_C, -C)
+	self.actor:incStat(game.player.STAT_A, -A)
 	self.actor:incStat(game.player.STAT_G, -G)
 	return true
 end
